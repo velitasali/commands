8b48e7113dc9d32852da50411e307591ce6d8580
Veli Tasalı <veli.tasali@gmail.com>
2022-06-07 14:48:55 +0000

    test: Remove demo tests

--- tests/web.rs
+++ tests/web.rs
@@ -5,16 +5,9 @@
 extern crate wasm_bindgen_test;
 use wasm_bindgen_test::*;
 
-use libcommands::get_number;
-
 wasm_bindgen_test_configure!(run_in_browser);
 
 #[wasm_bindgen_test]
 fn test_passes() {
     assert_eq!(1 + 1, 2);
-}
-
-#[wasm_bindgen_test]
-fn test_numbers_equal() {
-    assert_eq!(get_number(), 42);
 }
